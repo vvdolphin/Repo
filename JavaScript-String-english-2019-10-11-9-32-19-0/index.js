50ad09a61e45e2878198e65810a373bf4311b732
vvdolphin <1273027520@qq.com>
2020-06-19 14:44:55 +0800

    first

---
+++ JavaScript-String-english-2019-10-11-9-32-19-0/index.js
@@ -0,0 +1,14 @@
+// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+var name = 'hello';
+// TODO write your code here...
+console.log(name.toUpperCase());
+
+// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+var sentence = 'good afternoon, mr mike.';
+//TODO write your code here...
+console.log(sentence.replace('g','G')).replace('a','A').replace('m','M');
+
+// Complete the following procedure to extract the amount of money variable, output: 20.
+var money = '￥20';
+//TODO write your code here...
+console.log(money.substring(1));
